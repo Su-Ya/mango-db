320ab417617f1c57e27b7af1bfd0eab8a0ee1283
mangoSu <acid7820@gmail.com>
2026-02-25 22:30:54 +0800

    fix: enable trailingSlash in next.config.ts to resolve next/link 404s on github pages

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   },
   basePath: "/mango-db",// github repo name, 告訴 Next 所有的路由預設加這一層
   assetPrefix: "/mango-db/",// github repo name, 告訴 Next 所有的靜態檔案抓取路徑加這一層
+  trailingSlash: true, // 強制產生 /index.html 資料夾結構，解決 GitHub Pages 找不到檔案的問題
   /* config options here */
 };
 
