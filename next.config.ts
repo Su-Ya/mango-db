e1d81ac4da8d2e1710feb7a703bb3e07e5a4b91b
mangoSu <acid7820@gmail.com>
2026-02-25 20:10:41 +0800

    ci: add GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/mango-db",// github repo name, 告訴 Next 所有的路由預設加這一層
+  assetPrefix: "/mango-db/",// github repo name, 告訴 Next 所有的靜態檔案抓取路徑加這一層
   /* config options here */
 };
 
